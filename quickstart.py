85e7e17abc979cca2596435e7a047f83f1d4f43f
Vedantjaiswal4352 <69847543+Vedantjaiswal4352@users.noreply.github.com>
2023-01-28 15:38:45 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -5,9 +5,7 @@ from httplib2 import Http
 
 def main():
     """Hangouts Chat incoming webhook quickstart."""
-    url = 'https://chat.googleapis.com/v1/spaces/7D8la4AAAAE/messages?key=' \
-          'AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=' \
-          'HulDviWFMz6R5FW-GwlO4INCW1cZGDDExuWVaEbbr_g%3D'
+    url = 'https://chat.googleapis.com/v1/spaces/AAAAcvkG2xI/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=IYegoiqxTz8zaqt9qkM1AjltnFZOeAnHO0bessuSMFM%3D'
     bot_message = {
         'text': 'Hello from a Python script!'}
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
